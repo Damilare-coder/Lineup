d573b525da17f25ae9f78393844ae413b05069a3
Damilare Oyinloye <oyinloyedamilare12@gmail.com>
2025-05-18 23:52:29 +0100

    I added local storage to temporarily save manager's information

--- signup.js
+++ signup.js
@@ -1,5 +1,5 @@
-document.addEventListener('keydown', (e)=>{
-    if(e.key === 'Enter') {
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
         signUp();
     }
 });
@@ -25,18 +25,42 @@ const toast = (text, background, color, position = 'right') => {
 let allManagers = []
 
 const signUp = () => {
-    if (coach.value =='' || email.value=='' || club.value=='' || password.value==''){
+    if (coach.value == '' || email.value == '' || club.value == '' || password.value == '') {
         toast('Not that quick 😁, fill in the inputs', '#f00', '#fff')
         sub.innerHTML = '...loading'
         setTimeout(() => {
-            sub.innerHTML = 'Submit'
+            sub.innerHTML = 'Create account'
         }, 1000)
 
-    } else{
-        sub.innerHTML = '...loading'
-        setTimeout(() =>{
-            sub.innerHTML ='submit'
-        }, 1500)
+    } else {
 
-    }
+        sub.innerHTML = '...loading'
+        setTimeout(() => {
+            sub.innerHTML = 'Create account'
+        }, 1500)
+        console.log(allManagers);
+        
+
+    
+
+
+    const mName = document.getElementById('coach').value;
+    const mail = document.getElementById('email').value;
+    const mClub = document.getElementById('club').value;
+    const pass = document.getElementById('password').value;
+
+    const userObj = { mName, mail, mClub, pass }
+
+    allManagers.push(userObj)
+
+    document.getElementById('coach').value = ''
+    document.getElementById('email').value = ''
+    document.getElementById('club').value = ''
+    document.getElementById('password').value = ''
+
+    localStorage.users = JSON.stringify(allManagers)
+
+    // window.location.href = 'signin.html'
+
+}
 }
